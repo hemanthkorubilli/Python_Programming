6ed9746c0a0c1b74410d2091491d0711f664ef5d
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-25 12:32:46 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,10 @@
+"""here all the basics of python are covered"""
+str="#"
+a="2"
+b=-2
+c=5
+print(str*6)
+print((a+str)*6)
+"""In modulo division when the denominator is negative only then the answer will be neagtive"""
+print(b%c)
+print(c%b)
