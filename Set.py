e248f7f29fc98393433cbfc0a5b8343032cb373f
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-26 17:27:12 +0530

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,8 @@
+# set is the collection of the unordered type
+# Each element in the set must be unique and immutable
+ref={1,2,3,4}
+print(ref)
+#empty set
+#set={}#empty dictionary
+collection=set()#empty set
+print(type(collection))
