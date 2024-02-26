291be9fe9bd9080e2fd90a9c0e08ad0f7e5c16fe
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-26 11:14:51 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,9 @@
+# Dictionary is used to  store data values in key-value pairs
+# So basically a dictionary is changable ,duplicates are not allowed
+thisdict1={
+    "name":"Hemanth",
+    "age":2,
+}
+thisdict1.update({"age":22})
+thisdict1["name"]="varaha"
+print(thisdict1)
---
+++ exampleDic.py
@@ -0,0 +1,8 @@
+classdict = {
+    "Section" : "A",
+    "Roll Number":"203j1a0476",
+    "Name":"Hemanth",
+    "hobbies":["singing","dancing","writing stories"],
+}
+classdict.update({"name":"Rakesh"})
+print(classdict)
