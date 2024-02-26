e1053547eba0c3f13d81b255eb3eee1df7fe653a
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-26 06:49:04 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,11 @@
+"""list in python is similar to arrays """
+# list in python can be wrtten as
+# example:  marks=[1,2,3,4,5]
+"""The main difference in List in python is we can store elements of different data types together
+for example:  ["hemanth",33,90.78,"rakesh"]"""
+List=[20,30,40,50]
+for i in range(0,4):
+    print(List[i])
+print(List)
+List1=["hemanth",22,"prakash",40]
+print(List1)
