0e8abc1a45e45c22969e4fdf700ddf06ee99fe00
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-26 07:26:21 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,10 @@
+# tuples in python is also same as list but these are immmutable.
+# We cannot change the tuple as we do inlist
+#Syntax of tuple is: values=(1,2,3,4,5)
+tup=(2,4,1,2,5,6)
+print(tup)
+print(tup.index(4))
+print(tup.count(2))
+tup1=(2,3,'hemanth',30.6)
+print(tup1)
+
