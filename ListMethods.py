acb100b660081d583782255b55e4cfd667209629
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-26 07:13:58 +0530

    Add files via upload

---
+++ ListMethods.py
@@ -0,0 +1,19 @@
+lst=[2,4,5,3,6,70]
+#append function add the value at the end of the list
+lst.append(3)
+print(lst)
+#sort function automatically sorts the list in ascending order
+lst.sort(reverse=True)
+print(lst)
+#reverse function is used to reverse the list
+lst.reverse()
+print(lst)
+#insert method is used to add any value in desired index
+lst.insert(2,45)
+print(lst)
+#remove method removes the 1st occurance of our desired value
+lst.remove(3)
+print(lst)
+#pop function is used to remove thr desired value with its index
+lst.pop(3)
+print(lst)
