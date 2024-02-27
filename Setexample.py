425d848d3303ed763c2c430b72c0357722ac09a0
hemanthkorubilli <130943446+hemanthkorubilli@users.noreply.github.com>
2024-02-27 07:17:05 +0530

    Add files via upload

---
+++ Setexample.py
@@ -0,0 +1,9 @@
+# store 9 and 9.0 in a set
+set={9,9.0}
+print(set)
+# in python 9 and 9.0 are same ...
+# so we should store such values using their data types
+set1={
+    ("float",9.0),("int",9)
+}
+print(set1)
